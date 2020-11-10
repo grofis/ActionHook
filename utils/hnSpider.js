80df319c3cd4e1d5ce15476505afb371a5b74608
Mac <1216226589@qq.com>
2020-11-10 14:47:52 +0800

    [Hacker News] 提交代码。

--- utils/hnSpider.js
+++ utils/hnSpider.js
@@ -52,10 +52,10 @@ function formateData(arr) {
 			});
       */
       		const title = `Hacker News ${time.getTitleHour()}`;
-      		const contentTxt = `JSON.stringify(contents) \n JSON.stringify(titles.join('\r\n'))`
-      		console.log('test is:'+JSON.stringify(contents))
+      		const contentTxt = `${contents} \n${titles.join('\r\n')}`
+      		console.log('test is:'+contentTxt)
 
-		    issue.post(JSON.stringify(contentTxt), title, 'Hacker');
+		    // issue.post(JSON.stringify(contentTxt), title, 'Hacker');
 			// console.log('contentTxt:'+contentTxt)
 
 			// log打印内容
